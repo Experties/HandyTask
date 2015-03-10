1ef7391bbe79d07d20e44367659b9d48113b56e9
Hetang Shah <hetang.shah@gmail.com>
2015-03-09 17:02:22 -0700

    Login Screen style

--- app/src/main/java/experties/com/handytask/fragments/LoginFragment.java
+++ app/src/main/java/experties/com/handytask/fragments/LoginFragment.java
@@ -6,6 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.PhoneNumberUtil.PhoneNumberFormat;
+import com.google.i18n.phonenumbers.Phonenumber;
+import com.google.i18n.phonenumbers.Phonenumber.PhoneNumber;
 
 import experties.com.handytask.R;
 
@@ -13,9 +22,52 @@ import experties.com.handytask.R;
  * Created by hetashah on 3/7/15.
  */
 public class LoginFragment extends Fragment {
+    private EditText edTxtPhone;
+    private Button btnLogin;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.login_fragment, parent, false);
+        edTxtPhone = (EditText) v.findViewById(R.id.edTxtPhone);
+        btnLogin = (Button) v.findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                switch (v.getId()) {
+                    case R.id.btnLogin:
+
+                        String phoneNumber = edTxtPhone.getText().toString();
+
+                        PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
+                        PhoneNumber phNumberProto = null;
+                        long notFormatted = 0;
+                        try {
+                            phNumberProto = phoneUtil.parse(phoneNumber, "US");
+                            notFormatted = phNumberProto.getNationalNumber();
+                        } catch (NumberParseException e) {
+                            System.err.println("NumberParseException was thrown: " + e.toString());
+                        }
+
+                        boolean isValid = phoneUtil.isValidNumber(phNumberProto);
+
+                        if (isValid) {
+                            String username = String.valueOf(notFormatted);
+                            Toast.makeText(
+                                    v.getContext(),"Phone number is valid: " + username,
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(
+                                    v.getContext(),"Phone number is not valid: " + phoneNumber,
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        break;
+
+                }
+            }
+        });
         return v;
     }
 
--- app/src/main/java/experties/com/handytask/fragments/RegisterFragment.java
+++ app/src/main/java/experties/com/handytask/fragments/RegisterFragment.java
@@ -11,10 +11,14 @@ import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.i18n.phonenumbers.NumberParseException;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
@@ -28,9 +32,22 @@ import experties.com.handytask.R;
  * Created by hetashah on 3/7/15.
  */
 public class RegisterFragment extends Fragment {
-    private Spinner state;
     private String phoneNumber;
+
+    private ImageView imgVwProfile;
+
+    private Spinner sprState;
+
     private EditText edVwPhoneNo;
+    private EditText edTxtFirstName;
+    private EditText edTxtLastName;
+    private EditText edTxtAddress1;
+    private EditText edTxtAddress2;
+    private EditText edTxtCity;
+    private EditText edTxtZipCode;
+
+    private Button cancelBtn;
+    private Button signUpBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
@@ -40,25 +57,57 @@ public class RegisterFragment extends Fragment {
     }
 
     private void setupView(View v) {
-        state = (Spinner) v.findViewById(R.id.sprState);
+        //myButton.getBackground().setAlpha(0.5f);
+
+        sprState = (Spinner) v.findViewById(R.id.sprState);
         ArrayAdapter<CharSequence> stateAdapter = ArrayAdapter.createFromResource(
                 v.getContext(), R.array.state_arrays, R.layout.spinner_item);
         stateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        state.setAdapter(stateAdapter);
+        sprState.setAdapter(stateAdapter);
         //state.setSelection(spImageSizeAdapter.getPosition(settings.getImgSize()));
-
+        imgVwProfile = (ImageView) v.findViewById(R.id.imgVwProfile);
+        edTxtFirstName = (EditText)v.findViewById(R.id.edTxtFirstName);
+        edTxtLastName = (EditText)v.findViewById(R.id.edTxtLastName);
+        edTxtAddress1 = (EditText)v.findViewById(R.id.edTxtAddress1);
+        edTxtAddress2 = (EditText)v.findViewById(R.id.edTxtAddress2);
+        edTxtCity = (EditText)v.findViewById(R.id.edTxtCity);
+        edTxtZipCode = (EditText)v.findViewById(R.id.edTxtZipCode);
         edVwPhoneNo = (EditText)v.findViewById(R.id.edVwPhoneNo);
         if(phoneNumber != null) {
             edVwPhoneNo.setText(phoneNumber);
             edVwPhoneNo.setSelection(phoneNumber.length());
+            edTxtFirstName.setFocusableInTouchMode(true);
+            edTxtFirstName.requestFocus();
+            InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(edTxtFirstName, InputMethodManager.SHOW_IMPLICIT);
         }
+
+
+
+        cancelBtn = (Button)v.findViewById(R.id.cancelBtn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                edTxtFirstName.setText("");
+                edTxtLastName.setText("");
+                edTxtAddress1.setText("");
+                edTxtAddress2.setText("");
+                edTxtCity.setText("");
+                edTxtZipCode.setText("");
+                imgVwProfile.setImageResource(R.drawable.ic_profilee);
+                Toast.makeText(v.getContext(),"Entries got cleared",Toast.LENGTH_LONG).show();
+            }
+        });
+        signUpBtn = (Button)v.findViewById(R.id.signUpBtn);
+
     }
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TelephonyManager tMgr = (TelephonyManager)getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+        TelephonyManager tMgr = (TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
         phoneNumber = tMgr.getLine1Number();
         try {
             PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
